76e3f6dd0178c684c3655a4973b0b7aefbe5fcc5
lee14k <kmkovach@mtu.edu>
2024-01-26 17:44:27 -0500

    setting up db file

---
+++ db.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
+
+const connection = mysql.createConnection({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+});
+
+module.exports = connection;
